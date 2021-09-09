8e7fe96869b9548ff267526d45d4427c22a839b8
LucDubs <lucas.dubs5@gmail.com>
2021-09-09 12:55:38 -0300

    Aula JS - arquivos iniciais

---
+++ main.js
@@ -0,0 +1,118 @@
+
+// EVENTOS E MANIPULAÇÕES DE ELEMENTOS
+// function clicou(){
+//     document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+//     // console.log(document.getElementById("agradecimento"));
+//     // alert("obrigado por clicar");
+// }
+
+// function redirecionar(){
+//     window.open("https://www.youtube.com/"); //abre por fora
+//     window.location.href = "https://www.youtube.com"; //abre na msm guia
+// }
+
+//MUDAR ELEMENTO AO PASSAR E TIRAR MOUSE
+// function trocar(elemento){
+//     document.getElementById("mouse").innerHTML = "Obrigado por passar o mouse";
+//     elemento.innerHTML = "Obrigado por passar o mouse";
+//     // alert("trocar texto");
+// }
+
+// function voltar(elemento){
+//     // document.getElementById("mouse").innerHTML = "Passe o mouse novamente";
+//     elemento.innerHTML ="Passe o mouse novamente";
+// }
+
+// function load(){
+//     alert("página carregada");
+// }
+
+// function funcaoChange(elemento){
+//     console.log(elemento.value);
+// }
+
+
+//FUNÇÕES
+// function soma(n1, n2){
+//     return (n1 + n2);
+// }
+// alert(soma(10, 10));
+
+// var validar;
+// function validaIdade(idade){
+//     if (idade >= 18){
+//         validar = true
+//     }else{
+//         validar = false
+//     }
+// }
+// var idade = prompt("Qual a sua idade");
+// validaIdade(idade)
+// console.log(validar);
+
+
+/*DATA*/
+// var d = new Date();
+// alert(d.getMinutes());
+// alert(d);
+// alert(d.getSeconds());
+
+
+// ESTRUTURA DE REPETIÇÃO FOR
+// var count;
+// for (count=0; count <= 5; count++) {
+//     alert(count);
+// };
+
+
+// ESTRUTURA DE REPETIÇÃO WHILE
+// var count = 0;
+// while (count <= 5){
+//     console.log(count);
+//     alert(count);
+//     count++;
+// }
+
+
+// CONDICIONAL
+// var idade = prompt("Qual a sua idade");
+// if(idade >= 18){
+//     alert("Maior de idade");
+// }else{
+//     alert("Menor de idade");
+// };
+
+
+//LISTA DE DICIONÁRIOS
+// var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxo"}]
+// console.log(frutas);
+// alert(frutas[1].nome);
+
+
+//DICIONÁRIO
+// var fruta = {nome:"maçã", cor:"vermelha"}
+// console.log(fruta);
+// alert(fruta.cor);
+
+
+// // ARRAY
+// var lista = ["maçã", "pêra", "laranja"];
+// lista.push("uva");
+// lista.pop();
+
+// console.log(lista);
+// console.log(lista.toString());
+// console.log(lista.join(" - "));
+// console.log(lista.length);
+
+
+//VARIÁVEIS + MÉTODOS
+// var nome = "Lucas de Sousa";
+// var idade1 = 5;
+// var idade2 = 3;
+// var frase = "Japão é o melhor time do mundo";
+// alert(nome + " tem " + idade1 + " anos ");
+// alert(idade1 + idade2);
+// console.log(nome);
+// console.log(idade1 * idade2);
+// console.log(frase.toLowerCase());
